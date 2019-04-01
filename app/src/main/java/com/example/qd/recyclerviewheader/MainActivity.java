e0b082f223f974173c6fa7934b3b7534bd1a091f
wuqingsen <1243411677@qq.com>
2019-04-01 11:25:06 +0800

    '2019.4.1.11:25'

--- app/src/main/java/com/example/qd/recyclerviewheader/MainActivity.java
+++ app/src/main/java/com/example/qd/recyclerviewheader/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.qd.recyclerviewheader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.andview.refreshview.XRefreshView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.xRefreshView)
+    XRefreshView xRefreshView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
